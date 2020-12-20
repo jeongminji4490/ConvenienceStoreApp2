034f41dd238aa177a92a45a7c4930de98336bbbd
jeongminji4490 <hamins97@naver.com>
2020-12-21 00:03:48 +0900

    파이어베이스 연동 및 회원가입/로그인 구현

--- app/src/main/java/com/example/conveniencestoreapp/JoinActivity.java
+++ app/src/main/java/com/example/conveniencestoreapp/JoinActivity.java
@@ -1,15 +1,26 @@
 package com.example.conveniencestoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class JoinActivity extends AppCompatActivity {
 
     Button joinOkBtn;
+    private EditText name_join,phone_join,email_join,password_join;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +28,35 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         joinOkBtn=(Button)findViewById(R.id.joinOkBtn);
+        name_join=(EditText)findViewById(R.id.name_join);
+        phone_join=(EditText)findViewById(R.id.phone_join);
+        email_join=(EditText)findViewById(R.id.email_join);
+        password_join=(EditText)findViewById(R.id.phone_join);
+
+        firebaseAuth=FirebaseAuth.getInstance();
 
         joinOkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(JoinActivity.this,MainActivity2.class);
-                startActivity(intent);
+                final String email=email_join.getText().toString().trim();
+                final String password=password_join.getText().toString().trim();
+                final String name=name_join.getText().toString().trim();
+                final String phoneNum=phone_join.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent intent=new Intent(JoinActivity.this,MainActivity2.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(JoinActivity.this,"등록 에러",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/conveniencestoreapp/LoginActivity.java
+++ app/src/main/java/com/example/conveniencestoreapp/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.example.conveniencestoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button joinBtn,OkBtn;
+    private EditText EditEmail,EditPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,9 @@ public class LoginActivity extends AppCompatActivity {
 
         joinBtn=(Button)findViewById(R.id.joinBtn);
         OkBtn=(Button)findViewById(R.id.OkBtn);
+        EditEmail=(EditText)findViewById(R.id.EditEmail);
+        EditPassword=(EditText)findViewById(R.id.EditPassword);
+        firebaseAuth=FirebaseAuth.getInstance(); //FirebaseAuth의 인스턴스를 가져옴
 
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +43,23 @@ public class LoginActivity extends AppCompatActivity {
         OkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,MainActivity2.class);
-                startActivity(intent);
+                String email=EditEmail.getText().toString().trim();
+                String password=EditPassword.getText().toString().trim();
+                //String형 변수 email.password(edittext에서 받아오는 값)으로 로그인하는것
+
+                //이메일과 비밀번호가 공백일 경우에도 오류메세지를 보여주어야 함!
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){ //성공했을 때
+                            Intent intent=new Intent(LoginActivity.this,MainActivity2.class);
+                            startActivity(intent);
+                        }
+                        else{ //실패했을 때
+                            Toast.makeText(LoginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
