85bd50e27e4f5b396ff5b80dd80d72c0bddca62e
jeongminji4490 <hamins97@naver.com>
2020-12-21 23:46:48 +0900

    Gs25 행사상품 프레그먼트 구현,나머지 메뉴들도 프레그먼트 추가해야함, JoinActivity 오류 토스트 메세지 확인

--- app/src/main/java/com/example/conveniencestoreapp/CsMenuActivity.java
+++ app/src/main/java/com/example/conveniencestoreapp/CsMenuActivity.java
@@ -2,13 +2,28 @@ package com.example.conveniencestoreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CsMenuActivity extends AppCompatActivity {
+
+    Button gsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cs_menu);
+
+        gsBtn=(Button)findViewById(R.id.gsBtn);
+
+        gsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CsMenuActivity.this,FragmentMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
